6c7875b434919073b48af444c5f167c884295c03
Debbie Vo <debbienvo@yahoo.com>
2019-02-13 12:52:00 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Debbie Vo on 2/13/19.
+//  Copyright © 2019 debbienvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

dc5cb73d99e6f04869b051e9e671da2d376ce53f
Debbie Vo <debbienvo@yahoo.com>
2019-02-24 21:45:44 -0800

    Completion of required user stories for a2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Debbie Vo on 2/24/19.
+//  Copyright © 2019 debbienvo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
